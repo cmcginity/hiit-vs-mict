e3fd588b73899af878cc14f93b328183cd9da016
Curtis McGinity <curtis.mcginity+github@gmail.com>
2023-10-02 20:53:44 -0700

    updated svc account credentials

--- apps/study-dash-streamlit-test/pages/workout_view.py
+++ apps/study-dash-streamlit-test/pages/workout_view.py
@@ -26,10 +26,9 @@ ptest = os.path.join(pdata_myphd,'_processed','sourcetype_device','WearableFitbi
 
 # Google Drive API Initialization
 def setup_drive():
-    SERVICE_ACCOUNT_FILE = '/Users/curtismcginity/Downloads/hiit-vs-mict-50dbc00f450b.json'
     SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
 
-    creds = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+    creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=SCOPES)
     service = build('drive', 'v3', credentials=creds)
     return service
 
